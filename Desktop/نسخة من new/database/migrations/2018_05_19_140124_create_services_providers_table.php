b6b87825797aa8bc58122d41ebff8b2fe6cb8b11
Reem-Alarrouj <rmroma1994@gmail.com>
2018-05-19 17:21:03 +0300

    create DB for students and services providers

---
+++ Desktop/نسخة من new/database/migrations/2018_05_19_140124_create_services_providers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services_providers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->bigInteger('mobile');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('services_providers');
+    }
+}
